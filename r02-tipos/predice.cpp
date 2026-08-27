e56f61358e1bd6c0aecb0550bfd8e58d101e2072
carohgcia <A01648557@tec.mx>
2026-08-27 10:10:57 -0600

    R2: prediccion de conversiones

---
+++ r02-tipos/predice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 7/2 << endl;
+    cout << 7/2.0 << endl;
+    cout << 7%2 << endl;
+    cout << static_cast<int>(3.99) << endl;
+    cout << (1 == 1.0) << endl;
+    unsigned int u = 3;
+    std::cout << u - 5 << endl;
+
+    return 0;
+}
+
